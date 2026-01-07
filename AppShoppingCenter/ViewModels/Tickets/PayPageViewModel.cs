744b8d64c2314b786b4815355aa895ff6935194b
Yago S Santos <yago@fantury.com.br>
2026-01-07 17:59:36 -0300

    feat: Implementação QR Code

--- AppShoppingCenter/ViewModels/Tickets/PayPageViewModel.cs
+++ AppShoppingCenter/ViewModels/Tickets/PayPageViewModel.cs
@@ -1,12 +1,42 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using AppShoppingCenter.Models;
+using CommunityToolkit.Mvvm.ComponentModel;
 
-namespace AppShoppingCenter.ViewModels.Tickets
+namespace AppShoppingCenter.ViewModels.Tickets;
+
+[QueryProperty(nameof(Ticket), "ticket")]
+public partial class PayPageViewModel : ObservableObject
 {
-    class PayPageViewModel
+
+    private double HourValue = 0.07;    
+    private Ticket ticket;
+    public Ticket Ticket
     {
+        get { return ticket; }
+        set
+        {
+            GenerateDateOutAndTolerance(value);
+            GeneratePrice(value);
+            SetProperty(ref ticket, value);
+        }
+    }
+
+    private void GenerateDateOutAndTolerance(Ticket ticket)
+    {
+        var random = new Random();
+        var hour = random.Next(0, 12);
+        var minute = random.Next(0, 60);
+
+        ticket.DateOut = ticket.DateIn.AddHours(hour).AddMinutes(minute);
+        ticket.DateTolerance = ticket.DateOut.AddMinutes(30);
+    }
+
+
+
+    private void GeneratePrice(Ticket ticket)
+    {
+        var dif = ticket.DateOut.Ticks - ticket.DateIn.Ticks;
+        var difTS = new TimeSpan(dif);
+        
+        ticket.Price = difTS.TotalMinutes* HourValue;
     }
 }
